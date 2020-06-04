f865f1775964450a22056238ee956d71eb60dfa7
noahrslavik <noahrobertslavik@gmail.com>
2020-06-04 10:13:44 -0400

    First commit

---
+++ src/main/java/co/grandcirucs/TicketmasterApiApp/TicketmasterApiAppApplication.java
@@ -0,0 +1,13 @@
+package co.grandcirucs.TicketmasterApiApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketmasterApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketmasterApiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/grandcirucs/TicketmasterApiApp/TicketmasterApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.grandcirucs.TicketmasterApiApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketmasterApiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

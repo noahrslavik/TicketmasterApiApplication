201c3693dd91dfcc0fd4ec6835f89b77d91e7540
noahrslavik <noahrobertslavik@gmail.com>
2020-06-05 14:01:51 -0400

    Addding ticket repsonse

--- src/main/java/co/grandcirucs/TicketmasterApiApp/Dao/TicketmasterApiService.java
+++ src/main/java/co/grandcirucs/TicketmasterApiApp/Dao/TicketmasterApiService.java
@@ -1,9 +1,15 @@
 package co.grandcirucs.TicketmasterApiApp.Dao;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import co.grandcirucs.TicketmasterApiApp.Entities.Classifications;
+import co.grandcirucs.TicketmasterApiApp.Entities.Embedded;
+import co.grandcirucs.TicketmasterApiApp.Entities.Events;
+import co.grandcirucs.TicketmasterApiApp.Entities.Images;
 import co.grandcirucs.TicketmasterApiApp.Entities.TicketMaster;
 
 @Service
@@ -18,10 +24,29 @@ public class TicketmasterApiService {
 		
 		TicketMaster response = rest.getForObject(url, TicketMaster.class, ticketId, keyword);
 		System.out.println(response);
+		
+		Embedded embed = response.get_embedded();
+		List<Events> listOfEvents = embed.getEvents();
+		for(int i = 0; i < listOfEvents.size(); i++ ) {
+			String artistName = listOfEvents.get(i).getName();
+			String urlIn = listOfEvents.get(i).getUrl();
+			String locale = listOfEvents.get(i).getLocale();
+			String timezone = listOfEvents.get(i).getDates().getTimezone();
+			LocalDate localDate = listOfEvents.get(i).getDates().getStart().getLocalDate();
+			LocalTime localTime = listOfEvents.get(i).getDates().getStart().getLocalTime();
+			
+			String image = listOfEvents.get(i).getImages().get(2).getUrl();
+			String classifications  = listOfEvents.get(i).getClassifications().get(1).getGenre().getName();
+		
+			
+				
+				
+			
+		}
 		return response;
 	
+	}
 	}
 	
 	
 	
-}
---
+++ src/main/java/co/grandcirucs/TicketmasterApiApp/Entities/ConcertInfo.java
@@ -0,0 +1,11 @@
+package co.grandcirucs.TicketmasterApiApp.Entities;
+
+public class ConcertInfo {
+
+	
+	
+	
+	
+	
+	
+}
